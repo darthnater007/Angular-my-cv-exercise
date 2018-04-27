5fa564e7370c2bd40916084cf9fa168e72b938d3
darthnater007 <nsperk@gmail.com>
2018-04-27 15:12:33 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title: string = 'My CV';
+    name: string = 'Nate Perkins';
+    contactInfo: string = 'nsperk@gmail.com : 513-262-6710';
+workExperience = 'Cook, Barback, Bar Tender, Farm Hand, Warehouse, etc.';
+    
+    education = 'Ohio University | The Recording Workshop | Cincy CodeIT';
 }
