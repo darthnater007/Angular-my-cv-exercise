65132ea60b1d9873db097369208c0c468bd45dd8
darthnater007 <nsperk@gmail.com>
2018-04-27 23:57:03 -0400

    Added additional components to be rendered on the page, and added alot of goofy styling because it's late.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    WorkExperienceComponent
   ],
   imports: [
     BrowserModule
